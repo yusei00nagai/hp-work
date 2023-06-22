3a600d0011f40ba18e00d31fe4bbedd4f23d97d5
yusei00nagai <yusei.nagai1013@gmail.com>
2023-06-22 12:00:50 +0900

    aboutページ レスポンシブ対応

--- src/modules/home/About.tsx
+++ src/modules/home/About.tsx
@@ -9,12 +9,12 @@ export const About = () => {
 
       <img className="About-Content-Img" src={ AboutImg } alt="about-img"/>
 
-      <Box className="About-Content-Text">副業人材を主体とした<span>「業務委託サービス」</span>です。<br></br>
-        通常では人材派遣の市場に出てこないような大企業に勤めているベテランのエンジニアや開発の中核を担う<br></br>
+      <Box className="About-Content-Text">副業人材を主体とした<span>「業務委託サービス」</span>です。<br className="About-br"></br>
+        通常では人材派遣の市場に出てこないような大企業に勤めているベテランのエンジニアや開発の中核を担う<br className="About-br"></br>
         フリーランスエンジニアが<span>現在約100名</span>ほど所属しています。<p></p>
-        副業を主体としているため長時間の拘束稼働はできませんが、<br></br>
-        トップレベルの知識と経験を活かして複数人でチームとなってクライアントの業務を請け負います。<br></br>
-        クライアント側のチームメンバーと共同して開発を行うこともできるため、<br></br>
+        副業を主体としているため長時間の拘束稼働はできませんが、<br className="About-br"></br>
+        トップレベルの知識と経験を活かして複数人でチームとなってクライアントの業務を請け負います。<br className="About-br"></br>
+        クライアント側のチームメンバーと共同して開発を行うこともできるため、<br className="About-br"></br>
         <span>クライアント所属のエンジニアのスキルアップ</span>を行うことも可能です。<p></p>
       </Box>
     </Box>
@@ -44,7 +44,20 @@ const sx: SxProps<Theme> = {
   },
   ".About-Content-Img" : {
     width: "100%",
+    height: {
+      xs: "300px",
+      md: "100%",
+    },
+    objectPosition: {
+      xs: "20% 50%",
+      md: "100 100",
+    },
+    objectFit : {
+      xs: "cover",
+      md: "cover",
+    },
     paddingTop: "30px",
+    
   },
   "span" : {
     color: "#22A1B9",
@@ -54,9 +67,16 @@ const sx: SxProps<Theme> = {
     padding: "50px",
     fontFamiry: "Noto Sans",
     fontWeight: "400",
-    fontSize: "18px",
+    fontSize: {
+      xs: "16px",
+      md: "18px",
+    },
     lineHeight: "35px",
-    
-    
   },
+  ".About-br" : {
+    display: {
+      xs: "none",
+      md: "flex",
+    }
+  }
 };
