90910cba901a382da8ab2254bcff24a109f51d0c
yusei00nagai <yusei.nagai1013@gmail.com>
2023-06-26 15:21:55 +0900

    companyページ　追加
    レスポンシブ未対応

--- src/modules/home/Company.tsx
+++ src/modules/home/Company.tsx
@@ -5,47 +5,41 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
+import { ReactNode } from "react";
 
 function createData(
-  names: string[],
-  contents: string,
+  names: ReactNode,
+  contents: ReactNode,
 ) {
   return { names, contents};
 }
 
 const rows = [
-  createData(["会社名"], "株式会社テクノスタンダード"),
-  createData(['設立日'], "2022年6月"),
-  createData(["所在地"] , "東京都渋谷区渋谷3-10-15 YKビル7F"),
-  createData(['資本金'], "100万"),
-  createData(['代表取締役'], "加藤拓也"),
-  createData(['取引銀行',"取引先"], "三菱UFJ銀行`<br />`SBIネット銀行"),
-  // createData('取引先', "SBIネット銀行"),
-  // createData('', "株式会社バンタン"),
-  // createData('', "株式会社ジョブクラウン"),
-  // createData('', "株式会社メルメクス"),
-  // createData('', "マルナカ合同会社"),
+  createData(<Box>会社名</Box>, <Box>株式会社テクノスタンダード</Box>),
+  createData(<Box>設立日</Box>, <Box>2022年6月</Box>),
+  createData(<Box>所在地</Box>, <Box>東京都渋谷区渋谷3-10-15 YKビル7F</Box>),
+  createData(<Box>資本金</Box>, <Box>100万</Box>),
+  createData(<Box>代表取締役</Box>, <Box>加藤拓也</Box>),
+  createData(<Box>取引銀行<br />取引先</Box>, <Box>三菱UFJ銀行<br />SBIネット銀行<br />株式会社バンタン<br />株式会社ジョブクラウン<br />株式会社メルメクス<br />マルナカ合同会社</Box>),
 ];
 
 export const Company = () => {
   return (
     <Box className="Company" sx={sx}>
-      <Typography className="Company-Title"><span>A</span>BOUT</Typography>
-      <TableContainer >
-      <Table className="Company-Table">
-        <TableBody className="Company-Table-Body">
-          {rows.map((row) => (
-            <TableRow className="Company-Table-Row">
-              <TableCell component="th" scope="row"> {row.names.map((name) =>
-              { name<br /> }
-              )}
-            </TableCell>
-              <TableCell>{row.contents}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <Typography className="Company-Title"><span>C</span>OMPANY</Typography>
+      <Box className="Company-Title-Sub">会社概要</Box>
+      <TableContainer className="Company-TableContainer">
+        <Table className="Company-Table">
+          <TableBody className="Company-Table-Body">
+            {rows.map((row) => (
+              <TableRow className="Company-Table-Row">
+                <TableCell className="Company-Table-Cell-1" component="th" scope="row"> {row.names}</TableCell>
+                <TableCell className="Company-Table-Cell-2">{row.contents}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
@@ -53,26 +47,57 @@ export const Company = () => {
 const sx: SxProps<Theme> = {
   "&.Company" : {
     width: "100%",
-    backgroundColor: "#22A1B9"
+    backgroundColor: "#22A1B9",
+    textAlign: "center",
   },
   ".Company-Title" : {
     paddingTop: "72px",
-    textAlign: "center",
     color: "#FFFFFF99",
     fontFamiry: "Avenir Next",
     fontStyle: "normal",
     fontWeight: "700",
-    fontSize: "68px",
+    fontSize: {
+      xs: "40px",
+      md: "68px",
+    },
     lineHeight: "93px",
     letterSpacing: "0.15em",
   },
+  "span" : {
+    color: "#fff",
+  },
+  ".Company-Title-Sub" : {
+    marginBottom: "74px",
+    fontFamiry: "Noto Sans",
+    fontWeight: "700",
+    fontSize: "16px",
+    lineHeight: "22px",
+    letterSpacing: "0.1em",
+    color: "#fff",
+  },
+  ".Company-TableContainer" : {
+  },
   ".Company-Table" : {
-    align: "center",
+    width: "880px",
+    margin: "0 auto",
   },
   ".Company-Table-Body" : {
-    align: "center",
   },
   ".Company-Table-Row" : {
     "&:last-child td, &:last-child th": { border: 0 },
-  }
+  },
+  ".Company-Table-Cell-1" : {
+    textAlign: "left",
+    color: "#fff",
+    fontFamiry: "Noto Sans JP",
+    fontSize: "20px",
+    display: "flex",
+    alignItems: "top",
+  },
+  ".Company-Table-Cell-2" : {
+    textAlign: "left",
+    color: "#fff",
+    fontFamiry: "Noto Sans JP",
+    fontSize: "20px",
+  },
 };
