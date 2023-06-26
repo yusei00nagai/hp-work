678f4bd2eff97df4169a8d59dc3840c90a83c145
yusei00nagai <yusei.nagai1013@gmail.com>
2023-06-26 15:23:45 +0900

    mainvisualページ display:"none"効かない

--- src/modules/home/Mainvisual.tsx
+++ src/modules/home/Mainvisual.tsx
@@ -6,8 +6,8 @@ export const Mainvisual = () => {
     <Box className="Mainvisual" sx={sx}>
       <Box className="Mainvisulual-Img">
         <Box className="Mainvisual-Title">
-          <Box className="Maivisual-Title-1">安心<span>と</span>信頼<span>×</span></Box>
-          <Box className="Maivisual-Title-2">高度<span>な</span>技術力</Box>
+          <Box className="Maivisual-Title-1">安心<span>と</span>信頼<span className="Mainvisual-span-xs"><br className="Mainvisual-br" />×</span></Box>
+          <Box className="Maivisual-Title-2">高度<span>な<br className="Mainvisual-br" /></span>技術力</Box>
           <Box className="Mainvisual-Title-Sub">高品質な開発を標準に</Box>
         </Box>
       </Box>
@@ -27,28 +27,52 @@ const sx: SxProps<Theme> = {
     backgroundSize: "cover",
   },
   ".Mainvisual-Title" : {
-    padding: "277px 753px 92px 98px",
+    padding: {
+      md: "277px 753px 92px 98px",
+      xs: ""
+    },
     color: "#fff",
     fontFamiry: "Noto Sans JP",
     fontStyle: "normal",
     fontWeight: "700",
-    fontSize: "96px",
-    lineHeight: "100px",
+    fontSize: {
+      md: "96px",
+      xs: "57px",
+    },
+    // lineHeight: "100px",
     letterSpacing: "0.1em",
     textDecoration: "underline 1px solid #fff",
     textUnderlinePosition: "under",
   },
   "span" : {
-    fontSize: "64px",
+    fontSize: {
+      xs: "38px",
+      md: "64px",
+    },
+  },
+  ".Mainvisual-span-xs" : {
+    display : {
+      xs: "none",
+      md: "flex",
+    },
+  },
+  ".Mainvisual-br" : {
+    dispaly : {
+      md: "none",
+      xs: "flex",
+    },
+    // display: "none",
   },
   ".Mainvisual-Title-Sub" : {
     position: "absolute",
-    // padding: "542px 660px 92px 98px",
     color: "#fff",
     fontFamiry: "Noto Sans JP",
     fontStyle: "normal",
     fontWeight: "600",
-    fontSize: "48px",
+    fontSize: {
+      xs: "24px",
+      md: "48px",
+    },
     lineHeight: "150px",
     letterSpacing: "0.2em",
   }
